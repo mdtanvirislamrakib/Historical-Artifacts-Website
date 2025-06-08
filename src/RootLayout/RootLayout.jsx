fe4253102c7bab3b7febaf1f516e865fe00ee30a
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-06-08 18:09:30 +0600

    start historical artifacts website

---
+++ src/RootLayout/RootLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <header>
+
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+                
+            </footer>
+        </div>
+    );
+};
+
+export default RootLayout;
